b0e78e78fe6616def6aa34386c3fd3c7124b7b0a
Chandler Cook <chandleracook01@gmail.com>
2024-10-16 09:15:50 -0400

    UI-1.0.0

---
+++ src/main/java/tkm/window.java
@@ -0,0 +1,19 @@
+package tkm;
+
+import javax.swing.JFrame;
+
+public class window {
+
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Clue-Less");
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        
+    }
+
+}
