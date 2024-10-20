d12331166ccf50a655c17073771df4df9b0b3ea2
Justin <134976883+jknguyen97@users.noreply.github.com>
2024-10-19 21:10:08 -0700

    Add files via upload

---
+++ src/main/java/tkm/Card.java
@@ -0,0 +1,16 @@
+// created by justin 10/19/2024
+
+// game cards
+public class Card {
+    private String name;
+
+    // initialize the card name
+    public Card(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
