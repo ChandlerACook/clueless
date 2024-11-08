ae005b7750b54b983e06e43abdfa72f414880128
Justin <134976883+jknguyen97@users.noreply.github.com>
2024-11-08 07:58:02 -0800

    Update WeaponType.java
    fleshed out

--- src/main/java/tkm/enums/WeaponType.java
+++ src/main/java/tkm/enums/WeaponType.java
@@ -5,10 +5,24 @@ package tkm.enums;
  * @file WeaponType.java
  * @date 10/25/2024
  * @author Mike
- * 
  * This enum type represents the weapons available in Clue-Less
  */
 
 public enum WeaponType {
-    KNIFE, LEAD_PIPE, WRENCH, ROPE, CANDLESTICK, REVOLVER
+    KNIFE("Knife"),
+    CANDLESTICK("Candlestick"),
+    REVOLVER("Revolver"),
+    ROPE("Rope"),
+    LEAD_PIPE("Lead Pipe"),
+    WRENCH("Wrench");
+
+    private final String name;
+
+    WeaponType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
