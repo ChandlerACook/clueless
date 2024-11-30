a8b89c3fff2248ceba9eaa389d65c5032fa31864
smsnell <86450845+smsnell@users.noreply.github.com>
2024-11-30 16:53:10 -0500

    Add files via upload

--- src/main/java/tkm/ui/CardPanel.java
+++ src/main/java/tkm/ui/CardPanel.java
@@ -1,48 +1,37 @@
+
 package tkm.ui;
 
-import java.awt.GridLayout;
-
-import javax.swing.JComboBox;
-import javax.swing.JLabel;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
+import tkm.gamelogic.Card;
 
-import tkm.gamelogic.Player;
+/**
+ *
+ * @author Mike
+ */
 public class CardPanel extends JPanel {
-
-    //private JButton playerHand;
-    private JLabel panelLabel;
-    private JComboBox<String> playerHandDropdown;
-    private JLabel username;
-    private Player player;
-
-    public CardPanel(Player player) {
-        player = this.player;
-        this.initComponents(player);
-        this.setLayout(new GridLayout(0,1,5,5));
-        this.add(panelLabel);
-        //this.add(playerHandDropdown);
-        this.add(username);
+    
+    private final ArrayList<Card> hand;
+    private final ArrayList<JButton> cardButtons;
+    
+    public CardPanel(List<Card> playerHand) {
+        this.hand = (ArrayList<Card>) playerHand;
+        cardButtons = new ArrayList();
+        this.initComponents();
     }
-
-    private void initComponents(Player player) {
-        panelLabel = new JLabel("Player Hand");
-        panelLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        panelLabel.setVerticalAlignment(SwingConstants.NORTH);
-        /*
-        String[] cardNames = new String[player.getHand().size()];
-        for (int i = 0; i < player.getHand().size(); i++) {
-            cardNames[i] = player.getHand().get(i).getName(); // Get the card name from each Card object
+    
+    private void initComponents() {
+        this.setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
+        this.setBorder(BorderFactory.createTitledBorder("Your Cards"));
+        
+        for(Card card : hand) {
+            JButton button = new JButton(card.getName());
+            cardButtons.add(button);
+            this.add(button);
         }
-
-        playerHandDropdown = new JComboBox<>(cardNames); // Populate the dropdown with card names
-        playerHandDropdown.setAlignmentX(CENTER_ALIGNMENT);
-*/
-        username = new JLabel(player.getName());
     }
-/*
-    private JButton getPlayerHand() {
-        return playerHand;
-    }
-*/
 }
